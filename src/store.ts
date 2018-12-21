473c32b97df255d34e549effc81cd0dab402839e
David Banks <amoebae@gmail.com>
2018-12-21 11:04:34 +0000

    leftovers

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Vuex, { StoreOptions } from 'vuex';
-import mc from './mutation-constants';
-import { NodeFillMap, RootState } from './interfaces';
+import { RootState } from './interfaces';
 import graphView from './vuex-module';
 
 Vue.use(Vuex);
