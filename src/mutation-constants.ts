8425da8c3e78b412dde60960224ffd4cc4c8fd66
David Banks <amoebae@gmail.com>
2019-01-04 12:19:52 +0000

    version bump

--- src/mutation-constants.ts
+++ src/mutation-constants.ts
@@ -2,5 +2,5 @@ export default {
     SET_NODE_DND_TARGETS: 'setNodeDndTargets',
     SET_HOVERED_NODE_INDICES: 'setHoveredNodeIndices',
     SET_POSSIBLE_ROOTS: 'setPossibleRoots',
-    SELECT_ROOT: 'selectRoot'
+    SELECT_ROOT: 'selectRoot',
 };
