a468da8b7997fa994d620ee85be0d36f8fc55891
David Banks <amoebae@gmail.com>
2019-01-04 12:24:32 +0000

    set loglevel

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import * as log from 'loglevel';
+
+log.setLevel(log.levels.INFO);
 
 require('typeface-oxygen');
 
